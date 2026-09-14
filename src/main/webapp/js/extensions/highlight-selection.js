adef9b030e1c99864f311ee56fafd8f937a69187
PawelRoma <pawel.roman@pradma.pl>
2026-09-14 13:23:38 +0200

    feat: add highlight toggle via ext-cmd IPC

--- src/main/webapp/js/extensions/highlight-selection.js
+++ src/main/webapp/js/extensions/highlight-selection.js
@@ -1,62 +1,74 @@
 (function ()
 {
-    var HIGHLIGHT_CELL_COLOR = '#FF8000';
-    var HIGHLIGHT_EDGE_COLOR = '#FFB366';
-    var activeHighlights = [];
-    var graph = null;
+	var HIGHLIGHT_CELL_COLOR = '#FF8000';
+	var HIGHLIGHT_EDGE_COLOR = '#FFB366';
+	var activeHighlights = [];
+	var graph = null;
+	var highlightEnabled = true;
 
-    function clearHighlights()
-    {
-        for (var i = 0; i < activeHighlights.length; i++)
-        {
-            activeHighlights[i].destroy();
-        }
-        activeHighlights = [];
-    }
+	function clearHighlights()
+	{
+		for (var i = 0; i < activeHighlights.length; i++)
+		{
+			activeHighlights[i].destroy();
+		}
+		activeHighlights = [];
+	}
 
-    function highlightCell(cell, color, strokeWidth)
-    {
-        var state = graph.view.getState(cell);
-        if (!state) return;
-        var h = new mxCellHighlight(graph, color, strokeWidth);
-        h.highlight(state);
-        activeHighlights.push(h);
-    }
+	function highlightCell(cell, color, strokeWidth)
+	{
+		var state = graph.view.getState(cell);
+		if (!state) return;
+		var h = new mxCellHighlight(graph, color, strokeWidth);
+		h.highlight(state);
+		activeHighlights.push(h);
+	}
 
-    function onSelectionChange()
-    {
-        clearHighlights();
-        var cells = graph.getSelectionCells();
-        if (!cells || cells.length === 0) return;
+	function onSelectionChange()
+	{
+		if (!highlightEnabled) return;
+		clearHighlights();
+		var cells = graph.getSelectionCells();
+		if (!cells || cells.length === 0) return;
 
-        for (var i = 0; i < cells.length; i++)
-        {
-            highlightCell(cells[i], HIGHLIGHT_CELL_COLOR, 3);
-            var edges = graph.getEdges(cells[i], null, false, true, false);
-            if (edges)
-            {
-                for (var j = 0; j < edges.length; j++)
-                {
-                    highlightCell(edges[j], HIGHLIGHT_EDGE_COLOR, 2);
-                }
-            }
-        }
-    }
+		for (var i = 0; i < cells.length; i++)
+		{
+			highlightCell(cells[i], HIGHLIGHT_CELL_COLOR, 3);
+			var edges = graph.getEdges(cells[i], null, false, true, false);
+			if (edges)
+			{
+				for (var j = 0; j < edges.length; j++)
+				{
+					highlightCell(edges[j], HIGHLIGHT_EDGE_COLOR, 2);
+				}
+			}
+		}
+	}
 
-    window.Draw.loadPlugin(function (ui)
-    {
-        graph = ui.editor.graph;
-        graph.getSelectionModel().addListener(mxEvent.CHANGE, onSelectionChange);
+	window.Draw.loadPlugin(function (ui)
+	{
+		graph = ui.editor.graph;
+		graph.getSelectionModel().addListener(mxEvent.CHANGE, onSelectionChange);
 
-        ui.editor.addListener('pageSelected', function ()
-        {
-            clearHighlights();
-            if (graph)
-            {
-                graph.getSelectionModel().removeListener(onSelectionChange);
-            }
-            graph = ui.editor.graph;
-            graph.getSelectionModel().addListener(mxEvent.CHANGE, onSelectionChange);
-        });
-    });
+		ui.editor.addListener('pageSelected', function ()
+		{
+			clearHighlights();
+			if (graph)
+			{
+				graph.getSelectionModel().removeListener(onSelectionChange);
+			}
+			graph = ui.editor.graph;
+			graph.getSelectionModel().addListener(mxEvent.CHANGE, onSelectionChange);
+		});
+
+		window.electron.registerMsgListener('ext-cmd', function (msg)
+		{
+			if (msg.type !== 'highlight-toggle') return;
+			highlightEnabled = msg.value;
+			if (!highlightEnabled)
+			{
+				clearHighlights();
+			}
+		});
+	});
 })();
